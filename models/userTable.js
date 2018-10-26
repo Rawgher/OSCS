76e91fcfc181a54ac4484d293bb44e1be2e3e870
Sushichan <collin.chew.7188@gmail.com>
2018-10-25 21:11:22 -0700

    setup index

--- models/userTable.js
+++ models/userTable.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    user_name: { type: String, required: true },
-    user_firstName: { type: String, required: true },
-    user_lastName: { type: String, required: true },
-    user_pass: { type: String, required: true },
+    user_name: { type: String, required: true, trim: true },
+    user_firstName: { type: String, required: true, trim: true },
+    user_lastName: { type: String, required: true, trim: true },
+    user_pass: { type: String, required: true, trim: true },
     user_level: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
