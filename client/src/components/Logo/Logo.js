a18d37a5cdeaf67483709cdd679a6021fd982920
Enea <eneasecondary@gmail.com>
2018-10-26 21:25:34 -0700

    Drew a test icon

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Logo.css";
 // added import of image -RDP
-import logo from "./images/brain.png"
+import logo from "./images/BrainOutline.png";
 
 const Logo = () => (
-  <div className="logoWrapper">
-  {/* calling import to make image show -RDP */}
+  <div className="logoWrapper container">
+    {/* calling import to make image show -RDP */}
     <img src={logo} alt="Outline of Brain" />
   </div>
 );
