72eb4192676ba03a8fa1e9911860c057f9dda2b7
Enea <eneasecondary@gmail.com>
2018-10-27 21:23:11 -0700

    Fragmented the Search page so that the navbar can sit outside the Wrapper if needed.

--- client/src/pages/Search-Page/Search.js
+++ client/src/pages/Search-Page/Search.js
@@ -3,26 +3,27 @@ import "./Search.css";
 import PrimarySearchAppBar from "../../components/SearchBar";
 import { Col, Row, Container } from "../../components/Grid";
 import background from "./images/background.png";
-import Nav from "../../components/Nav";
 // import NavTabs from "../../components/Nav";
 
 const Search = () => (
-  <div className="EGA-search-wrapper">
-    <div className="EGA-background-image">
-      <img src={background} className="stretch" alt="Grey Background" />
+  <React.Fragment>
+    {/* <NavTabs className="EGA-NavTabs" /> */}
+    <h2 className="EGA-search-logo-title">ONE STOP CODE SHOP</h2>
+    <div className="EGA-search-wrapper">
+      <div className="EGA-background-image">
+        <img src={background} className="EGA-stretch" alt="Grey Background" />
+      </div>
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <div className="EGA-searchbar-div">
+              <PrimarySearchAppBar />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-    <Nav />
-    {/* <NavTabs /> */}
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <div className="EGA-searchbar-div">
-            <PrimarySearchAppBar />
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+  </React.Fragment>
 );
 
 export default Search;
