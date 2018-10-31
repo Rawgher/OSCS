78dc9b784b800babf63c8265539844f9c09706d3
elaine <user@host.com>
2018-10-31 16:13:30 -0700

    created userpage

--- client/src/pages/Forum-UserPage/UserPage.js
+++ client/src/pages/Forum-UserPage/UserPage.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Grid from '@material-ui/core/Grid';
-import "./UserPage.css";
 import { PostList, PostListItem } from "../../components/PostList";
+import BackBtn from "../../components/BackBtn";
+import "./UserPage.css";
 
 class UserPage extends Component {
+    
+    // TODO: get correct state key-value pairs!!
     state = {
-        // TODO: get array of user posts
         userPosts,
-        // TODO: get id of logged in user
-        userID: 1
+        userID: 1,
+        username: "blah",
+        firstName: "first",
+        lastName: "last",
+        userReplies
     };
 
     render() {
@@ -25,16 +30,54 @@ class UserPage extends Component {
                     spacing={24}
                 >
                     <Grid item xs={12} m={8} className="ESH_forum-col">
+                        <div className="ESH_body-title">POSTS</div>
                         <PostList>
+                            {/* TODO: find correct keys for post key-value pairing */}
                             {this.state.userPosts.map(post => (
                                 <PostListItem key={post._id}>
                                     <Link to={"/Forum/" + post._id}>
                                         {post.title}
                                     </Link>
+                                    {post.date}
+                                </PostListItem>
+                            ))}
+                        </PostList>
+                        <div className="ESH_body-title">COMMENTS</div>
+                        <PostList>
+                            {/* TODO: find correct keys for reply key-value pairing */}
+                            {this.state.userReplies.map(reply => (
+                                <PostListItem key={reply._id}>
+                                    <Link to={`/Forum/${reply.postID}`}>
+                                        {reply.postTitle}
+                                    </Link>
+                                    {reply.message}
+                                    {reply.date}
                                 </PostListItem>
                             ))}
                         </PostList>
                     </Grid>
+
+                    <Grid item xs={12} m={4} className="ESH_account-info">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    {/* TODO: check the state key-values for account info */}
+                                    <td className="acct-property">Username: </td>
+                                    <td>{this.state.username}</td>
+                                </tr>
+                                <tr>
+                                    <td className="acct-property">First Name: </td>
+                                    <td>{this.state.firstName}</td>
+                                </tr>
+                                <tr>
+                                    <td class="acct-property">Last Name: </td>
+                                    <td>{this.state.lastName}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Grid>
+
+                    <BackBtn />
 
                 </Grid>
             </Grid>
