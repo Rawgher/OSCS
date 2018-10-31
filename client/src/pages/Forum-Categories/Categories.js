8bdf54c718797338e46bbf2468d217578e017990
Enea <eneasecondary@gmail.com>
2018-10-30 18:04:21 -0700

    Added missing route pages and fixed all but one console error. Still outputting the manifest. Do we need it?

---
+++ client/src/pages/Forum-Categories/Categories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Categories.css";
+
+const Forum = () => (
+  <div>
+    <h2>Forum Page Placeholder</h2>
+  </div>
+);
+
+export default Forum;
---
+++ client/src/pages/Login-Page/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => (
+  <div>
+    <h2>Login Page Placeholder</h2>
+  </div>
+);
+
+export default Login;
