64b22a855683b4b6cd4b8b2a7a7459e751c8af9a
Enea <eneasecondary@gmail.com>
2018-11-06 21:24:16 -0800

    removed the word fluid from the Documentation container

--- client/src/pages/Documentation/Documentation.js
+++ client/src/pages/Documentation/Documentation.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Documentation from "../../components/Documentation";
+import Sidebar from "../../components/Sidebar";
+import Chat from "../../components/Chat";
 
 class DocumentationPage extends Component {
   render() {
     return (
-      <Container fluid>
+      <Container>
+        <Sidebar />
         <Row>
           <Col size="md-12" id="EGA-documentationPageMain">
             <Documentation />
           </Col>
         </Row>
+        <Chat />
       </Container>
     );
   }
