43b0d67e1d20a831e27d493aa3fd12174333e0f7
elaine <user@host.com>
2018-11-08 20:29:36 -0800

    fixed sidebar styling.

--- client/src/pages/Forum-Categories/Categories.js
+++ client/src/pages/Forum-Categories/Categories.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import Grid from '@material-ui/core/Grid';
-import BackBtn from "../../components/BackBtn";
 import axios from "axios";
 import "../../ESH_style.css";
 import "./Categories.css";
 import { Col, Row, Container } from "../../components/Grid";
-import Sidebar from "../../components/Sidebar";
+import ForumSidebar from "../../components/Forum-Sidebar";
 import Chat from "../../components/Chat";
 import NavTabs from "../../components/Nav";
 import Background from "../../components/Background";
@@ -17,17 +15,17 @@ class Categories extends Component {
     topics: []
   };
 
-  componentDidMount(){
-    axios.get('api/forum/categories').then(res=>{
-      this.setState({topic: res.data});
-    }).catch(err=>{
+  componentDidMount() {
+    axios.get('api/forum/categories').then(res => {
+      this.setState({ topic: res.data });
+    }).catch(err => {
       console.log("this is err=>", err);
     })
   }
 
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Container fluid>
           <Background />
           <Row>
@@ -36,43 +34,41 @@ class Categories extends Component {
             </Col>
           </Row>
         </Container>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          spacing={24}
-        >
-          <Grid item xs={12}>
-            <h4 className="ESH_main-title">FORUMS</h4>
-            <div className="ESH_line" />
-          </Grid>
-          <Grid item s={12} m={12} className="ESH_forum-col" spacing={24}>
-            <table style={{ width: 900 }}>
-              <tr className="th">
-                <th className="ESH_tcol1">TOPICS</th>
-                <th className="ESH_tcol2">POSTS</th>
-                <th className="ESH_tcol3">FRESHNESS</th>
-              </tr>
-              <tr>
-                <td>topic here adkfjasdfij alksdjfaoids fladkjf </td>
-                <td className="ESH_tcol2">20</td>
-                <td>11/3/2018</td>
-              </tr>
-              {/* TODO: find correct keys for mapping */}
-              {this.state.topics.map(topic => (
-                <tr>
-                  <td><a href={`/Forum/${topic._id}`}>{topic.topic_name}</a></td>
-                  <td className="ESH_tcol2">{topic.topic_posts}</td>
-                  <td>{topic.updatedAt}</td>
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <h4 className="ESH_main-title">FORUMS</h4>
+              <div className="ESH_line" />
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-9" className="ESH_forum-col">
+              <table style={{ width: "100%" }}>
+                <tr className="th">
+                  <th className="ESH_tcol1">TOPICS</th>
+                  <th className="ESH_tcol2">POSTS</th>
+                  <th className="ESH_tcol3">FRESHNESS</th>
                 </tr>
-              ))}
-            </table>
-          </Grid>
-        </Grid>
-        <Sidebar />
+                <tr>
+                  <td>topic here adkfjasdfij alksdjfaoids fladkjf </td>
+                  <td className="ESH_tcol2">20</td>
+                  <td>11/3/2018</td>
+                </tr>
+                {/* TODO: find correct keys for mapping */}
+                {this.state.topics.map(topic => (
+                  <tr>
+                    <td><a href={`/Forum/${topic._id}`}>{topic.topic_name}</a></td>
+                    <td className="ESH_tcol2">{topic.topic_posts}</td>
+                    <td>{topic.updatedAt}</td>
+                  </tr>
+                ))}
+              </table>
+            </Col>
+            <ForumSidebar />
+          </Row>
+        </Container>
         <Chat />
-      </div>
+      </React.Fragment>
     );
   }
 }
