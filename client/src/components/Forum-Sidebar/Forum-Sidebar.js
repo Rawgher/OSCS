92ecebf95008879ebb068855947f5ae1a92fc753
elaine <user@host.com>
2018-11-08 18:42:04 -0800

    fixed styling for forum sidebar

--- client/src/components/Forum-Sidebar/Forum-Sidebar.js
+++ client/src/components/Forum-Sidebar/Forum-Sidebar.js
@@ -6,13 +6,10 @@ import "./Forum-Sidebar.css";
 
 const ForumSidebar = () => (
     <Col size="md-3">
-        <Button variant="contained" size="large" color="primary" type="submit" name="action" id="create-post">
+        <Button variant="contained" size="large" color="primary" type="submit" name="action" id="create-post" href="/Forum/NewPost">
             CREATE POST
             <Icon>create</Icon>
         </Button>
-        <div class="ESH_create-post">
-            <a class="waves-effect waves-light btn ESH_create-btn" href="/Forum/NewPost">CREATE POST</a>
-        </div>
 
         <div class="ESH_top-posts">
             <div class="ESH_title">POPULAR POSTS</div>
