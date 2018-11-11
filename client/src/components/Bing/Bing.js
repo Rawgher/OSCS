023a200f60d6db3302f91443b284a397d56b712f
Roger <rdpender3@gmail.com>
2018-11-10 16:06:59 -0800

    commit before merge

--- client/src/components/Bing/Bing.js
+++ client/src/components/Bing/Bing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Bing.css";
 
 class Bing extends React.Component {
   render() {
@@ -7,11 +8,14 @@ class Bing extends React.Component {
     // make text white, name is a clickable link, add bottom white border
     // make title orange on hover
     return bing.map((a, id) => (
-      <div>
-        <div key={id}>
-          <h3>{a.url}</h3>
-          <h3>{a.snippet}</h3>
-          <h3>{a.name}</h3>
+      <div className="RDPleftAlign">
+        <div key={id} className="RDPwhiteBorder">
+          <a href={a.url} target="_blank" rel="noreferrer noopener">
+            <p className="RDPwhiteText RDPhoverLink RDPtitleSize RDPpaddingTop">
+              {a.name}
+            </p>
+          </a>
+          <p className="RDPwhiteText RDPpaddingBot">{a.snippet}</p>
         </div>
       </div>
     ));
