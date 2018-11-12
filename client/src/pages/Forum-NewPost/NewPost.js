d20f503408d98a28e01f0bfaef4fba78502e0914
SchneiderMan <jschneid94@gmail.com>
2018-11-11 17:58:13 -0800

    Save before merge

--- client/src/pages/Forum-NewPost/NewPost.js
+++ client/src/pages/Forum-NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 import TextField from "@material-ui/core/TextField";
@@ -50,75 +51,79 @@ class NewPost extends Component {
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <Container fluid>
-          <Background />
-          <Row>
-            <Col size="md-12">
-              <NavTabs />
-            </Col>
-          </Row>
-        </Container>
+    if (this.props.loggedIn === false) {
+      <Redirect to="/forum/categories" />;
+    } else {
+      return (
+        <React.Fragment>
+          <Container fluid>
+            <Background />
+            <Row>
+              <Col size="md-12">
+                <NavTabs />
+              </Col>
+            </Row>
+          </Container>
 
-        <Container>
-          <Row>
-            <Col size="md-12">
-              <h4 className="ESH_main-title">CREATE NEW POST</h4>
-              <div className="ESH_line" />
-            </Col>
-          </Row>
+          <Container>
+            <Row>
+              <Col size="md-12">
+                <h4 className="ESH_main-title">CREATE NEW POST</h4>
+                <div className="ESH_line" />
+              </Col>
+            </Row>
 
-          <Row>
-            <Col size="md-9" className="ESH_forum-col">
-              <form>
-                <div className="input-field">
-                  <input
-                    id="post_title"
-                    type="text"
-                    name="post_title"
-                    value={this.state.post_title}
-                    onChange={this.handleInputChange}
-                    placeholder="Post Title"
-                  />
-                  <label for="post_title" id="textarea1" className="active">
-                    Post Title
-                  </label>
-                </div>
-                <div className="input-field">
-                  <textarea
-                    id="textarea1"
-                    className="materialize-textarea"
-                    name="post_body"
-                    placeholder="Description"
-                  />
-                  <label id="textarea1" className="active">
-                    Description
-                  </label>
-                </div>
+            <Row>
+              <Col size="md-9" className="ESH_forum-col">
+                <form>
+                  <div className="input-field">
+                    <input
+                      id="post_title"
+                      type="text"
+                      name="post_title"
+                      value={this.state.post_title}
+                      onChange={this.handleInputChange}
+                      placeholder="Post Title"
+                    />
+                    <label for="post_title" id="textarea1" className="active">
+                      Post Title
+                    </label>
+                  </div>
+                  <div className="input-field">
+                    <textarea
+                      id="textarea1"
+                      className="materialize-textarea"
+                      name="post_body"
+                      placeholder="Description"
+                    />
+                    <label id="textarea1" className="active">
+                      Description
+                    </label>
+                  </div>
 
-                <Button
-                  variant="contained"
-                  size="large"
-                  type="submit"
-                  name="action"
-                  id="submit"
-                  onClick={this.handleFormSubmit}
-                >
-                  Submit
-                  <Icon style={{ marginLeft: 15 }}>send</Icon>
-                </Button>
-              </form>
+                  <Button
+                    variant="contained"
+                    size="large"
+                    type="submit"
+                    name="action"
+                    id="submit"
+                    onClick={this.handleFormSubmit}
+                  >
+                    Submit
+                    <Icon style={{ marginLeft: 15 }}>send</Icon>
+                  </Button>
+                </form>
 
-              <BackBtn />
-            </Col>
+                <BackBtn />
+              </Col>
 
-            <ForumSidebar />
-          </Row>
-          <Chat />
-        </Container>
-      </React.Fragment>
-    );
+              <ForumSidebar />
+            </Row>
+            <Chat />
+          </Container>
+        </React.Fragment>
+      );
+    }
   }
 }
 
