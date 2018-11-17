a1daa32e27cc49a28397351a5cf620bda86453fe
Roger <rdpender3@gmail.com>
2018-11-16 21:48:34 -0800

    fixed routing

--- client/src/App.js
+++ client/src/App.js
@@ -90,9 +90,48 @@ class App extends Component {
                 />
               )}
             />
-            <Route exact path="/forum/:id" component={Posts} />
-            <Route exact path="/forum/post/:id" component={ThisPost} />
-            <Route exact path="/forum/user/:id" component={User} />
+            <Route
+              exact
+              path="/forum/:id"
+              render={props => (
+                <Posts
+                  {...props}
+                  getUser={this.getUser}
+                  updateUser={this.updateUser}
+                  loggedIn={this.state.loggedIn}
+                  username={this.state.username}
+                  user_id={this.state.user_id}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/forum/post/:id"
+              render={props => (
+                <ThisPost
+                  {...props}
+                  getUser={this.getUser}
+                  updateUser={this.updateUser}
+                  loggedIn={this.state.loggedIn}
+                  username={this.state.username}
+                  user_id={this.state.user_id}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/forum/user/:id"
+              render={props => (
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  updateUser={this.updateUser}
+                  loggedIn={this.state.loggedIn}
+                  username={this.state.username}
+                  user_id={this.state.user_id}
+                />
+              )}
+            />
             <Route
               exact
               path="/search"
