4d36ef76977b347b59667c2deab6cde500443e06
Roger <rdpender3@gmail.com>
2018-11-19 20:07:00 -0800

    working on redirect. should be set up for when we have session storage

--- client/src/pages/Forum-ThisPost/ThisPost.js
+++ client/src/pages/Forum-ThisPost/ThisPost.js
@@ -7,14 +7,15 @@ import BackBtn from "../../components/BackBtn";
 import Background from "../../components/Background";
 import { Col, Row, Container } from "../../components/Grid";
 import ForumSidebar from "../../components/Forum-Sidebar";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import "./ThisPost.css";
 
 class ThisPost extends Component {
   state = {
     thispost: [],
     replies: [],
-    reply_body: ""
+    reply_body: "",
+    redirect: false
   };
 
   componentDidMount() {
@@ -49,109 +50,124 @@ class ThisPost extends Component {
     });
   };
 
+  routeChange = () => {
+    this.setState({
+      redirect: true
+    });
+  };
+
   // TODO: NOT WORKING
   handleFormSubmit = event => {
     event.preventDefault();
     // if (this.state.replies.reply_content && this.props.user_id) {
-      axios
-        .post("/api/forum/newreply/", {
-          reply_author: this.props.username,
-          reply_content: this.state.reply_body,
-          reply_post: this.state.thispost.post_subject
-        })
-        .then(function(res) {
-          res.redirect(`/forum/${this.state.thispost.post_id}`);
-        })
-        .catch(err => console.log(err));
+    axios
+      .post("/api/forum/newreply/", {
+        reply_author: this.props.username,
+        reply_content: this.state.reply_body,
+        reply_post: this.state.thispost.post_subject
+      })
+      .then(() => {
+        console.log("am i getting here?");
+        this.routeChange();
+        console.log(this.state.redirect);
+        // window.location.reload();
+      })
+      .catch(err => console.log(err));
     // }
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <Container fluid>
-          <Background />
-          <Row>
-            <Col size="md-12">
-              <NavTabs
-                updateUser={this.props.updateUser}
-                loggedIn={this.props.loggedIn}
-                username={this.props.username}
-              />
-            </Col>
-          </Row>
-        </Container>
+    const route = `/forum/${this.state.thispost._id}`;
 
-        <Container>
-          <Row>
-            <Col size="md-12">
-              <h4 className="ESH_main-title">
-                {this.state.thispost.post_topic} //{" "}
-                {this.state.thispost.post_subject}
-              </h4>
-              <div className="ESH_line" />
-            </Col>
-          </Row>
+    if (this.state.redirect) {
+      return <Redirect to={route} />;
+    } else {
+      return (
+        <React.Fragment>
+          <Container fluid>
+            <Background />
+            <Row>
+              <Col size="md-12">
+                <NavTabs
+                  updateUser={this.props.updateUser}
+                  loggedIn={this.props.loggedIn}
+                  username={this.props.username}
+                />
+              </Col>
+            </Row>
+          </Container>
 
-          <Row>
-            <Col size="md-9" className="ESH_forum-col">
-              <div className="ESH_body-title">
-                <b>{this.state.thispost.post_subject}</b>
-                <div style={{ fontStyle: "italic" }}>
-                  {this.convertDate(this.state.thispost.post_update)}
+          <Container>
+            <Row>
+              <Col size="md-12">
+                <h4 className="ESH_main-title">
+                  {this.state.thispost.post_topic} //{" "}
+                  {this.state.thispost.post_subject}
+                </h4>
+                <div className="ESH_line" />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col size="md-9" className="ESH_forum-col">
+                <div className="ESH_body-title">
+                  <b>{this.state.thispost.post_subject}</b>
+                  <div style={{ fontStyle: "italic" }}>
+                    {this.convertDate(this.state.thispost.post_update)}
+                  </div>
                 </div>
-              </div>
-              <p className="ESH_padding">{this.state.thispost.post_body}</p>
+                <p className="ESH_padding">{this.state.thispost.post_body}</p>
 
-              <div className="ESH_body-title">COMMENTS</div>
+                <div className="ESH_body-title">COMMENTS</div>
 
-              <ul class="ESH_user-posts">
-                {this.state.replies.map(reply => (
-                  <li>
-                    {reply.reply_content}
-                    <div className="ESH_comment-detail">
-                      on {this.convertDate(reply.reply_update)} by{" "}
-                      <Link to={`/forum/user/${reply.reply_author}`}>
-                        {reply.reply_author}
-                      </Link>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+                <ul class="ESH_user-posts">
+                  {this.state.replies.map(reply => (
+                    <li>
+                      {reply.reply_content}
+                      <div className="ESH_comment-detail">
+                        on {this.convertDate(reply.reply_update)} by{" "}
+                        <Link to={`/forum/user/${reply.reply_author}`}>
+                          {reply.reply_author}
+                        </Link>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
 
-              <form>
-                <div className="input-field">
-                  <textarea
-                    id="textarea1"
-                    placeholder="Write your comment here."
-                    class="materialize-textarea"
-                    name="reply_body"
-                    value={this.state.reply_body}
-                    onChange={this.handleInputChange}
-                  />
-                  <label id="textarea1" />
-                </div>
-                <Button
-                  variant="contained"
-                  size="large"
-                  type="submit"
-                  name="action"
-                  id="submit"
-                  onClick={this.handleFormSubmit}
-                >
-                  Submit
-                  <Icon style={{ marginLeft: 15 }}>send</Icon>
-                </Button>
-              </form>
+                <form>
+                  <div className="input-field">
+                    <textarea
+                      id="textarea1"
+                      placeholder="Write your comment here."
+                      class="materialize-textarea"
+                      name="reply_body"
+                      value={this.state.reply_body}
+                      onChange={this.handleInputChange}
+                    />
+                    <label id="textarea1" />
+                  </div>
+                  <Button
+                    variant="contained"
+                    size="large"
+                    type="submit"
+                    name="action"
+                    id="submit"
+                    onClick={this.handleFormSubmit}
+                  >
+                    Submit
+                    <Icon style={{ marginLeft: 15 }}>send</Icon>
+                  </Button>
+                </form>
 
-              <BackBtn />
-            </Col>
+                <BackBtn />
+              </Col>
 
-            <ForumSidebar loggedIn={this.props.loggedIn} />
-          </Row>
-        </Container>
-      </React.Fragment>
-    );
+              <ForumSidebar loggedIn={this.props.loggedIn} />
+            </Row>
+          </Container>
+        </React.Fragment>
+      );
+    }
   }
 }
 
