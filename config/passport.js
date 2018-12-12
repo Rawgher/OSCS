88df5d0779a357c0144faf4a884a3d26e076c6fd
Roger <rdpender3@gmail.com>
2018-12-11 22:21:50 -0800

    commented out passport lines

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,28 @@
-const passport = require('passport');
-const LocalStrategy = require('./localStrategy');
-const GithubStrategy = require('./githubStrategy');
-const GoogleStrategy = require('./googleStrategy');
+const passport = require("passport");
+const LocalStrategy = require("./localStrategy");
+// const GithubStrategy = require('./githubStrategy');
+const GoogleStrategy = require("./googleStrategy");
 const User = require("../models/userTable");
 
 // Called on login, saves the id to session req.session.passport.user
 passport.serializeUser((user, done) => {
-    console.log('*** serializeUser called, user: ', user);
-    console.log('------------');
-    done(null, { _id: user._id });
+  console.log("*** serializeUser called, user: ", user);
+  console.log("------------");
+  done(null, { _id: user._id });
 });
 
 // User object attaches to the request as req.user
 passport.deserializeUser((id, done) => {
-    console.log('Deserialize called')
-    User.findOne(
-        {_id: id },
-        'user_name',
-        (err, user) => {
-            console.log('*** Deserialize user, user:', user);
-            console.log('-------------');
-            done(null, user);
-        }
-    )
+  console.log("Deserialize called");
+  User.findOne({ _id: id }, "user_name", (err, user) => {
+    console.log("*** Deserialize user, user:", user);
+    console.log("-------------");
+    done(null, user);
+  });
 });
 
 passport.use(LocalStrategy);
 passport.use(GoogleStrategy);
-passport.use(GithubStrategy);
+// passport.use(GithubStrategy);
 
 module.exports = passport;
-
